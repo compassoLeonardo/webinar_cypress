3699b7d62d45260f8c076e836d3312230e092bd2
Leonardo Kartabil <leonardo.kartabil@compasso.com.br>
2022-06-06 15:18:07 -0300

    Add: Estrutura e2e Add: Estrutura para teste de login na Serverest

---
+++ cypress/e2e/serverest/login.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe('Testes sobre a aplicação front.serverest.dev', () => {
+    before('Realizar login na aplicação',() => {
+
+    })
+  
+    it('Deve validar o usuário cadastrado na lista de usuários', () => {
+      
+    })
+  
+  })
+  
